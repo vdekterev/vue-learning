dce58f39ec06b92698eb4d2b32503d2bce554844
vdekterev <vlad.decterev@icloud.com>
2024-10-19 20:42:51 +0400

    created small demo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router/index.js';
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App)
+	.use(router)
+	.mount('#app')
