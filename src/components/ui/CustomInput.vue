462739a9a5a311ec9e2e3eff47b2ce32dfedc567
vdekterev <vlad.decterev@icloud.com>
2024-11-20 22:48:31 +0400

    Animations | Test

---
+++ src/components/ui/CustomInput.vue
@@ -0,0 +1,58 @@
+<script setup>
+	const { placeholder, type, name, isInvalid }
+		= defineProps(['placeholder', 'type', 'name', 'isInvalid']);
+</script>
+
+<template>
+	<div class="custom_input">
+		<input
+			:placeholder="placeholder"
+			:type="type ?? 'text'"
+			:name="name"
+			class="custom_input__input"
+			:class="{'invalid': isInvalid}"
+		/>
+<!--		<div class="error-hint" x-show="false"></div>-->
+	</div>
+</template>
+
+<style scoped lang="scss">
+.custom_input {
+	position: relative;
+	&__input {
+		position: relative;
+		width: 100%;
+		padding: 16px;
+		background: rgba(242, 242, 242, 0.03);
+		border: none;
+		border-radius: 6px;
+		color:rgba(242, 242, 242, 0.50);
+		font-weight: 600;
+		font-size: 1rem;
+		transition: all 0.3s ease-in-out;
+		&:hover { background: rgba(242, 242, 242, 0.06); }
+		&::placeholder {
+			user-select: none;
+		}
+		&:focus,
+		&:focus-visible {
+			border: 1px solid rgba(242, 242, 242, 0.08) !important;
+			background: #141516 !important;
+		}
+	}
+
+	&__input.invalid {
+		border: 1px solid crimson !important;
+		color: crimson !important;
+		&::placeholder { color: crimson !important; }
+		&:focus { border: 1px solid var(--color-red) !important; }
+	}
+
+	.error-hint {
+		margin-top: 4px;
+		font-size: 12px;
+		color: var(--color-red);
+	}
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,93 @@
+<script setup>
+import { ref } from 'vue';
+import CustomInput from '@/components/ui/CustomInput.vue';
+
+const guests = [
+	{
+		id: 1,
+		name: 'Guest One'
+	},
+	{
+		id: 2,
+		name: 'Guest Two'
+	},
+];
+
+const invitedGuests = ref(guests);
+
+function addGuest(e) {
+	let guestName = e.target.value.trim();
+	if (guestName === '') {
+		return;
+	}
+	invitedGuests.value.unshift({
+		id: Math.random(),
+		name: guestName,
+	})
+
+	e.target.value = '';
+}
+</script>
+
 <template>
 	<div>
 		<h1>Home Page</h1>
+
+		<div class="container">
+			<CustomInput
+				:name="'test'"
+				:placeholder="'Add person to invite...'"
+				@keydown.enter="addGuest"
+			/>
+		</div>
+
+		<ul>
+			<TransitionGroup name="invitees">
+				<li v-for="guest in invitedGuests" :key="guest.id">
+					{{ guest.name }}
+				</li>
+			</TransitionGroup>
+		</ul>
 	</div>
 </template>
+
+<style scoped lang="scss">
+	.container {
+		margin-top: 2rem;
+	}
+
+	ul {
+		font-size: 2rem;
+		margin-top: 1rem;
+		display: grid;
+		gap: .5rem;
+		grid-template-columns: repeat(auto-fill, minmax(30%, 1fr));
+
+		li {
+			display: flex;
+			padding: 8px;
+			border: 1px solid #bed600;
+			border-radius: 8px;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+
+	}
+
+	.invitees {
+		&-enter-from {
+			opacity: 0;
+			transform: translateY(-100%);
+		}
+		&-enter-to {
+			opacity: 1;
+			transform: translateY(0);
+		}
+		&-enter-active {
+			transition: opacity 1s, transform .5s;
+		}
+		&-move {
+			transition: all 2s ease-in-out;
+		}
+	}
+</style>
