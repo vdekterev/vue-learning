2e4738e0cf38d56e737d181594e372e9c8857b46
vdekterev <vlad.decterev@icloud.com>
2024-11-23 18:48:27 +0400

    ShowsApp | Init commit

---
+++ src/components/GameOfThrones.vue
@@ -0,0 +1,16 @@
+<script setup>
+	import axios from 'axios';
+
+	const response = await axios.get('https://thronesapi.com/api/v2/Characters');
+	console.log(response.data);
+</script>
+
+<template>
+	<h1>Game of Thrones!</h1>
+
+	{{ response }}
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
+<script setup>
+
+import GameOfThrones from '@/components/GameOfThrones.vue';
+</script>
+
 <template>
 	<div>
 		<h1>Home Page</h1>
+		<section>
+
+			<Suspense>
+
+				<GameOfThrones />
+
+				<template #fallback>
+					<h1>Loading!</h1>
+				</template>
+
+			</Suspense>
+
+		</section>
 	</div>
 </template>
